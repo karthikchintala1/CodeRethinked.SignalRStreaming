44048d700535b0b64a5bbeee57d8a259ab9af39a
Karthik Chintala <karthikreddy509@gmail.com>
2018-08-28 08:02:09 +0530

    signalr init

---
+++ CodeRethinked.SignalRStreaming/SignalR/StreamHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CodeRethinked.SignalRStreaming.SignalR
+{
+    public class StreamHub : Hub
+    {
+        public ChannelReader<int> DelayCounter(int delay)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            _ = WriteItems(channel.Writer, 20, delay);
+
+            return channel.Reader;
+        }
+
+        private async Task WriteItems(ChannelWriter<int> writer, int count, int delay)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                //For every 5 items streamed, add twice the delay
+                if (i % 5 == 0)
+                    delay = delay * 2;
+
+                await writer.WriteAsync(i);
+                await Task.Delay(delay);
+            }
+
+            writer.TryComplete();
+        }
+    }
+}
